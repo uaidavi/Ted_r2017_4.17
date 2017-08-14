a555f130ba0ba5d55d1f8cc7f62ac69ef6a40b6f
DESKTOP-6JTF7CM\daviu <daviuai25@gmail.com>
2017-08-14 18:41:38 -0300

    0.0.0 Último estado do projeto antigo. Pronto para limpeza.

---
+++ Intermediate/Build/Win32/TED_r2017/Shipping/UELinkerFixups/PCH.UELinkerFixupsName.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\Users\daviu\Documents\Ted_r2017_1v2\Intermediate\Build\Win32\UE4\Shipping\UELinkerFixupsName.h"
---
+++ Intermediate/Build/Win32/TED_r2017/Shipping/UELinkerFixups/UELinkerFixupsName.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_UELINKERFIXUPSNAME_H__
+#define __AUTO_UELINKERFIXUPSNAME_H__
+//Last Write: 12/9/2016 2:33:29 PM +00:00
+#include "C:\Users\daviu\Documents\Ted_r2017_1v2\Intermediate\Build\Win32\UE4\Shipping\UELinkerFixupsName.h"
+#endif//__AUTO_UELINKERFIXUPSNAME_H__
